6f514111dc2dfc8149d9fe07a3d448d2c8f3f4e0
kiryl <kirillzhenu@gmail.com>
2021-05-12 23:06:32 +0300

    Рефакторинг первого задания.
    1. Добавил валидацию ввода в функцию парсера.
    2. Разделил открытие файлов и основную логику.
    3. Разделил сериализатор на два отдельных класса.

--- Task1/script.py
+++ Task1/script.py
@@ -5,9 +5,23 @@ import json
 import xml.etree.ElementTree as ET
 
 
-class Serializer(object):
+class JSONSerializer(object):
     """Аттрибутом класса является словарь со студентами, который получается после объединения двух файлов.
-    Методы класса сериализуют полученный список либо в xml, либо в json и создают соответсвующий файл."""
+    Метод класса сериализует полученный список в json и создаёт соответсвующий файл."""
+
+    def __init__(self, students_dict_to_serialize):
+        self.students_dict_to_serialize = students_dict_to_serialize
+
+    def create_json(self):
+        """Полученный json будет иметь следующий вид:
+        {"Room #0": ["William Perez", ...], ...}"""
+        with open('result.json', 'w') as file:
+            json.dump(self.students_dict_to_serialize, file)
+
+
+class XMLSerializer(object):
+    """Аттрибутом класса является словарь со студентами, который получается после объединения двух файлов.
+       Метод класса сериализует полученный список в xml и создаёт соответсвующий файл."""
 
     def __init__(self, students_dict_to_serialize):
         self.students_dict_to_serialize = students_dict_to_serialize
@@ -36,29 +50,19 @@ class Serializer(object):
         with open('result.xml', 'w') as file:
             file.write(mydata)
 
-    def create_json(self):
-        """Полученный json будет иметь следующий вид:
-        {"Room #0": ["William Perez", ...], ...}"""
-        with open('result.json', 'w') as file:
-            json.dump(self.students_dict_to_serialize, file)
 
-
-def put_students_in_rooms(file_with_rooms, file_with_students):
+def put_students_in_rooms(room_list, student_list):
     """Возвращает словарь, который нужно сериализовывать.
-    Сначала из файла с комнатами выносятся все номера комнат.
+    Сначала из cписка с комнатами выносятся все номера комнат.
     Пары полученного словара представляют собой номер комнаты и пустые списки, куда будут заносится студенты.
-    Затем имена студентов из файла со студентами добавляются в соответствующие списки."""
+    Затем имена студентов из списка со студентами добавляются в соответствующие списки."""
     dict_of_rooms_with_students = {}
-    with open(f"{file_with_rooms}", "r") as list_of_rooms:
-        list_of_rooms = json.load(list_of_rooms)
-        for i in range(len(list_of_rooms)):
-            room_number = list_of_rooms[i]['name']
-            dict_of_rooms_with_students[room_number] = []
+    for i in range(len(room_list)):
+        room_number = room_list[i]['name']
+        dict_of_rooms_with_students[room_number] = []
 
-    with open(f"{file_with_students}", "r") as list_of_students:
-        list_of_students = json.load(list_of_students)
-        for student in list_of_students:
-            dict_of_rooms_with_students[f"Room #{student['room']}"].append(student['name'])
+    for student in student_list:
+        dict_of_rooms_with_students[f"Room #{student['room']}"].append(student['name'])
     return dict_of_rooms_with_students
 
 
@@ -68,26 +72,20 @@ def create_parser():
         -путь к файлу с комнатами
         -формат сериализации(xml или json)"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--students')
-    parser.add_argument('-r', '--rooms')
-    parser.add_argument('-f', '--format')
+    parser.add_argument('-s', '--students', type=argparse.FileType('r'))
+    parser.add_argument('-r', '--rooms', type=argparse.FileType('r'))
+    parser.add_argument('-f', '--format', choices=['json', 'xml'])
     return parser
 
 
 if __name__ == '__main__':
     my_parser = create_parser()
     args = my_parser.parse_args()
-    json_with_students = args.students
-    json_with_rooms = args.rooms
+    list_of_students = json.load(args.students)
+    list_of_rooms = json.load(args.rooms)
+    result_dict = put_students_in_rooms(list_of_rooms, list_of_students)
 
-    try:
-        result_dict = put_students_in_rooms(json_with_rooms, json_with_students)
-        serializer = Serializer(result_dict)
-        if args.format == 'xml':
-            serializer.create_xml()
-        elif args.format == 'json':
-            serializer.create_json()
-        else:
-            print('Incorrect format. Print "xml" or "json".')
-    except FileNotFoundError:
-        print('Incorrect path to file/files.')
+    if args.format == 'xml':
+        XMLSerializer(result_dict).create_xml()
+    elif args.format == 'json':
+        JSONSerializer(result_dict).create_json()
