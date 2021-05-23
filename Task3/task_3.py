9b176a0a8c1a620471bff8c38f4e0f160a8ce738
kiryl <kirillzhenu@gmail.com>
2021-05-23 21:45:30 +0300

    Переделал 3 таску

--- Task3/task_3.py
+++ Task3/task_3.py
@@ -1,26 +1,40 @@
 from threading import Thread, Lock
+import queue
 
-a = 0
+
+class ThreadLocker(object):
+    def __init__(self, lock):
+        self.lock = lock
+
+    def __enter__(self):
+        self.lock.acquire()
+
+    def __exit__(self, *args):
+        self.lock.release()
 
 
-def function(arg, lock):
-    global a
+def function(arg, lock, a, result_queue):
 
     for _ in range(arg):
-        lock.acquire()
-        a += 1
-        lock.release()
+        with ThreadLocker(lock):
+            a += 1
+
+    result_queue.put(a)
 
 
 def main():
+    a = 0
+    result_queue = queue.Queue()
     lock = Lock()
     threads = []
     for i in range(5):
-        thread = Thread(target=function, args=(1000000, lock,))
+        thread = Thread(target=function, args=(1000000, lock, a, result_queue))
         thread.start()
         threads.append(thread)
 
     [t.join() for t in threads]
+    while not result_queue.empty():
+        a += result_queue.get()
     print("----------------------", a)  # ???
 
 
